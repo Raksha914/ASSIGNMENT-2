01ef70f106b01addbf8c57e541b809c207029663
Raksha <rakshaa818@gmail.com>
2024-09-19 22:44:02 +0530

    third folder is added

---
+++ 3/arithmeticoperators.js
@@ -0,0 +1,15 @@
+// arithmatic operators
+
+let a = 10;
+let b = 5;
+let result = a + b;
+let result1 = a - b;
+let result2 = a * b;
+let result3 = a % b;
+let result4 = a / b;
+
+console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
