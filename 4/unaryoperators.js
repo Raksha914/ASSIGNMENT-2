011cdfb692458061247f847c59ffc9bbc313a374
Raksha <rakshaa818@gmail.com>
2024-09-19 23:16:01 +0530

    4th folder is added

---
+++ 4/unaryoperators.js
@@ -0,0 +1,16 @@
+// increment and decrement operators
+let x = 10;
+
+// Post increment
+console.log(x++); // 10
+console.log(x); // 11
+
+//Post decrement
+console.log(x--); // 11
+console.log(x); // 10
+
+//Pre increment
+console.log(++x); // 11
+
+//Pre decrement
+console.log(--x); //10
