08b8e63acb67bdcf0bf3fabd81322317ece0bac1
Raksha <rakshaa818@gmail.com>
2024-09-19 23:38:17 +0530

    5th folder is added

---
+++ 5/comparisionoperators.js
@@ -0,0 +1,35 @@
+//comparision operator
+
+let a = 20;
+let b = 10;
+
+//equal to
+console.log(a == b); // false
+console.log(b == b); // true
+
+//strict equal to
+console.log(a === b); //false
+
+//not equal to
+console.log(a != b); // true
+
+//strict not equal to
+console.log(a !== b); // true
+console.log(a !== 20); // false
+
+//greater than
+console.log(a > b); // true
+console.log(b > a); // false
+
+//lesse than
+console.log(a < b); // false
+console.log(b < a); // true
+
+//greater than or equal to
+console.log(a >= b); // true
+console.log(a >= 20); // true
+console.log(a > 25); // false
+
+//smaller than or equal to
+console.log(a <= b); // false
+console.log(a <= 25); // true
