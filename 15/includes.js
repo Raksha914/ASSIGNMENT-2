5b8751ea242135a08dfd6be185a87dfe593c87f6
Raksha <rakshaa818@gmail.com>
2024-09-20 18:35:10 +0530

    15th folder is added

---
+++ 15/includes.js
@@ -0,0 +1,5 @@
+//includes : cheks if the string conatins a specified value
+
+let str = "to check if a specific word exists in the string";
+console.log(str.includes("specific")); // true
+console.log(str.includes("value")); // false
