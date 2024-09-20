b53924571747ef7fd351606fafa87607baec815f
Raksha <rakshaa818@gmail.com>
2024-09-20 10:49:40 +0530

    9th folder is added

---
+++ 9/stringvariable.js
@@ -0,0 +1,19 @@
+let text = "Hello World"; //length is 11
+
+console.log(text.length); //11
+
+let ans = text.charAt(0);
+console.log(ans); //H
+
+ans = text.charAt(0);
+console.log(ans); //H
+
+console.log(text.charAt(0)); //H =>first letter
+
+let result = text.charAt(10);
+console.log(result); // d
+
+result = text.charAt(10);
+console.log(result); // d
+
+console.log(text.charAt(10)); //d => last letter
