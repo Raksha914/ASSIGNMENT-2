e175b4b4cb3d0892590260863f25f4d42e088d52
Raksha <rakshaa818@gmail.com>
2024-09-20 17:27:54 +0530

    13th folder is added

---
+++ 13/trimmedstring.js
@@ -0,0 +1,6 @@
+// removing extra spaces at the beginning and end
+
+let str = "  Hello, Mayank what is your future plan  ";
+console.log(str);
+
+console.log(str.trim()); //it will remove the whitespaces from both ends of string
