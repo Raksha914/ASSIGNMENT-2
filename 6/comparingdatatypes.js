d414f16acdba2986c5fde0e4203c9ece4cce0aac
Raksha <rakshaa818@gmail.com>
2024-09-20 07:45:40 +0530

    6th folder is added

---
+++ 6/comparingdatatypes.js
@@ -0,0 +1,6 @@
+// comparing data types
+let x = 5;
+let y = "5";
+console.log(x == y); //true equal=> checks only the value
+console.log(x === y); // false strict equal not only checks the value but also checks the data type
+console.log(typeof y); // string
