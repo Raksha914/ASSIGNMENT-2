28d4f791036489a7541cb3c01746c205335ab670
Raksha <rakshaa818@gmail.com>
2024-09-20 11:17:08 +0530

    10th folder is added

---
+++ 10/uppercase&lowercase.js
@@ -0,0 +1,13 @@
+// uppercase and lowercase
+
+let str = "Hello World, Welcome To JavaScript!";
+
+console.log(str.toUpperCase()); // HELLO WORLD, WELCOME TO JAVASCRIPT!
+
+str = str.toUpperCase();
+console.log(str); // HELLO WORLD, WELCOME TO JAVASCRIPT!
+
+str = str.toLowerCase();
+console.log(str); // hello world, welcome to javascript!
+
+console.log(str.toLowerCase()); // hello world, welcome to javascript!
