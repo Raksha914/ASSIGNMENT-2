96324df1cfc24310d87fd844cd85179f3560a27e
Raksha <rakshaa818@gmail.com>
2024-09-20 18:17:13 +0530

    14th folder is added

---
+++ 14/string into array.js
@@ -0,0 +1,5 @@
+// convert string into array of word
+
+let fruits = "Apple,Banana,Grape,Orange,Pomogranate";
+let str = fruits.split(","); // it will split the string into an array
+console.log(str); // ['Apple', 'Banana', 'Grape', 'Orange', 'Pomogranate']
