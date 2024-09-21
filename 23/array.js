0edaa18006a70f56a3e6d5f415578cff2b3c6364
Raksha <rakshaa818@gmail.com>
2024-09-21 11:42:03 +0530

    23rd folder is added

---
+++ 23/array.js
@@ -0,0 +1,12 @@
+// log 1st and last element in an array
+
+const numbers = [10, 20, 30, 40, 50];
+
+const length = numbers.length;
+console.log(length); // 5
+
+const length1 = numbers[0];
+console.log(length1); // 10
+
+const length2 = numbers[numbers.length - 1];
+console.log(length2); // 50
