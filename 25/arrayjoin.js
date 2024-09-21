53026cbddd60c8e63c108e42cc62808ac4ce9c68
Raksha <rakshaa818@gmail.com>
2024-09-21 12:43:29 +0530

    25th folder is added

---
+++ 25/arrayjoin.js
@@ -0,0 +1,6 @@
+// joing all the words into sentences by giving spaces
+
+let words = ["Hello", "world", "this", "is", "JavaScript"];
+
+let sentance = words.join(" "); // it will join all the array into single string
+console.log(sentance); // Hello world this is JavaScript
